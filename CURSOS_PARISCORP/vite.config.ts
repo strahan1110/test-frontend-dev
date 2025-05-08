5a8c2aa98df1674d9b609c2ce3e62a9cc5d0910d
Jemima Jange <u17302707@utp.edu.pe>
2025-05-08 18:43:20 -0500

    FIRST_COMMIT_PO
    COMMIT_ESTE_SÍ

---
+++ CURSOS_PARISCORP/vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/", // Asegura que las rutas funcionen correctamente en Vercel
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  assetsInclude: ["**/*.woff2", "**/*.woff", "**/*.ttf", "**/*.otf"],
+  optimizeDeps: {
+    include: ["react", "react-dom"]
+  }
+});
